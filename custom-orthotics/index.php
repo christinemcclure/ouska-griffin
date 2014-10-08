266d52695fbe441f92ae6b0b2c3021bfe8fd0817
Christine McClure <camcclure@gmail.com>
2014-10-07 19:47:30 -0500

    added stub and navigation for orthotics page

---
+++ custom-orthotics/index.php
@@ -0,0 +1,13 @@
+<?php include("../includes/html-head.html"); ?>
+  
+<body>
+
+<?php include("../includes/header-and-nav.html"); ?>    
+ 
+
+    
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="../bower_components/foundation/js/foundation.min.js"></script>
+<script src="../js/app.js"></script>
+</body>
+</html>
