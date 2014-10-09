a91bffc1f3305ede2074922008406551adb03910
Christine McClure <camcclure@gmail.com>
2014-10-08 20:32:31 -0500

    changed home page to use relative paths to include files

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php include("/includes/html-head.html"); ?>
+<?php include("includes/html-head.html"); ?>
   
 <body>
 
-<?php include("/includes/header-and-nav.html"); ?>    
+<?php include("includes/header-and-nav.html"); ?>    
  
 <div class="row">
   <div class="large-12 columns"><h1>Where will your feet take you today?</h1></div>
