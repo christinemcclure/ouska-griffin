63508bc5199274f136be5d321e8256e7ee03f12c
Christine McClure <camcclure@gmail.com>
2014-10-09 20:25:56 -0500

    walking page

--- walking-for-fun-and-fitness/index.php
+++ walking-for-fun-and-fitness/index.php
@@ -3,9 +3,65 @@
 <body>
 
 <?php include("../includes/header-and-nav.html"); ?>    
- 
-
-    
+ <div class="row">
+  <div class="large-12 columns"><h1>Walking for fun and fitness</h1></div>
+ </div>
+  
+<div class="row">
+<div class="large-12 columns">
+<img class="inline-wrap" src="../images/walking-woman.jpg" alt="Photograph of woman walking along a lakefront.">
+  
+  <p>
+Walking for fun or fitness is a wonderful mild to moderate exercise. It is much less stressful to our larger joints-ankles knees and hips as we age. Not all of us want to be strapped to a “walking program,” we just like being out of doors and moving. Even a causal stroll offers health and mental benefits.
+  </p>
+  <p>
+Healthy feet and well-fitting shoes are two obvious requirements to engage even in casual walking and a major consideration in a strenuous walking program. Healthy feet means they are free of callouses, corns, ingrown toenails, warts, sores and infections caused by bacterial and fungus. It means they have good biomechanical foot health, i.e. relating to the bone structure and alignment, tendons and ligaments. A hands on professional podiatric evaluation typically determines what structural weaknesses might hinder or even prevent the enjoyment of a nice lunchtime walk in the park - or week long trek in the mountains. Based on this evaluation we can determine a plan of corrective action. It might include something as simple as rest, a change of shoes, therapy, or custom orthotics. Surgery is a last resort.    
+  </p>  
+</div>    
+</div>  
+  
+<div class="row">
+  <div class="large-3 columns">
+  <h4>Shoes</h4>
+  </div>
+  <div class="large-9 columns">
+        <p>
+          Our feet are “custom” each with their own traits and-“personality.” If housed in ill-fitting shoes, our feet let us know they are unhappy via a pain message. Walking for fun or fitness requires well-fitting, supportive shoes. Age, fitness level, weight, and activity will dictate styles of shoes best for you. 
+        </p>
+  </div>    
+</div>  
+<div class="row">
+  <div class="large-3 columns">
+  <h4>What constitutes a good walking shoe?</h4>
+  </div>
+  <div class="large-9 columns">
+        <p>
+          This is not an easy question to answer. According to one professional society,</p> 
+        <blockquote>To date there has not been a reliable, repeatable methodology of footwear assessment that meets the standards of evidenced based medicine….</blockquote>
+        <p>The current commonly used terms of stability and motion control are poorly chosen and inappropriately used.” American Academy of Podiatric Sports Medicine web site.
+        </p>
+        <p>
+          With this in mind any supportive shoe will have a firm midsole. To test for midsole strength hold a shoe with both hands at heel and toe and try to compress the shoe. If it bends at the ball of the foot and not the midsole it has adequate support. If the shoe bends in the midsole it is not a well-made shoe and should not be used for exercise. It should have at least a 5 eye lace up; and ample heel height (about 1.5 – 2 inches.) Also a quality shoe should have a firm heel counter, the back structure of any shoe that holds the heel of the foot.
+        </p>
+        <p>
+          If you have custom orthotics take them when shopping for shoes. The existing liner of the shoe should be removed to accommodate the orthotic insert.
+        </p>
+  </div>    
+</div>  
+  
+<div class="row">
+  <div class="large-3 columns">
+  <h4>Conservative care starts with an actual problem being evident</h4>
+  </div>
+  <div class="large-9 columns">
+        <p>
+Few of us have perfect feet and many of us have minor structural defects. We do not overprescribe, over treat or “discover” a problem where none exists. If you are walking pain free odds are your feet are happy and do not require physician intervention even if there is a minor structural defect. If suddenly you have a painful foot for no oblivious reason, ask yourself have I begun wearing new shoes lately? If so stop wearing them and see if the pain fades away.  If not, then by all means see a professional without delay.
+        </p>
+  </div>    
+</div>  
+  
+  
+</div>      
 <script src="../bower_components/jquery/dist/jquery.min.js"></script>
 <script src="../bower_components/foundation/js/foundation.min.js"></script>
 <script src="../js/app.js"></script>
