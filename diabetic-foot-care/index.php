2dd2b85cb87d0bbebb5ecf23bc4f71c397761407
Christine McClure <camcclure@gmail.com>
2014-10-11 16:15:30 -0500

    image and text for diabetic page

--- diabetic-foot-care/index.php
+++ diabetic-foot-care/index.php
@@ -3,6 +3,55 @@
 <body>
 
 <?php include("../includes/header-and-nav.html"); ?>    
- 
 
+ <div class="row">
+  <div class="large-12 columns"><h1>Diabetic foot care</h1></div>
+ </div>
+  
+<div class="row">
+  <div class="large-12 columns">
+    <img src="../images/diabetic-foot-exam.jpg" class="inline-wrap-right" alt="">
+    <p>Dr. Ouska-Griffin has extensive training in caring for diabetic feet. We provide routine diabetic foot care of painful foot corns and callouses, treat ulcers, infections, and prescribe diabetic shoes with cushioned insoles. </p>
+    <p>Diabetic foot diagnoses by Dr. Ouska-Griffin includes:</p>
+    <ul class="indented">
+      <li>Patient interview</li>
+      <li>Checking of pulses</li>
+      <li>Inspection of skin</li>
+      <li>Testing for nerve sensitivity</li>
+    </ul>
+    <p>
+      For those of us with diabetes foot infections are all too common and potentially serious. One of the frequent reasons for diabetics to be admitted into the hospital is foot infection. Diabetes can cause poor circulation in our feet as well as nerve damage leading to ulcers or tissue damage or if left untreated gangrene and loss of toes or feet. Good management of daily blood glucose (sugar) level is important and should be monitored between your internist, endocrinologist, and podiatrist. Poor circulation can make a foot problem very complicated and resistive to treatment. In most cases problems with diabetic feet can be prevented with good at home care, a thorough medical evaluation and close monitoring by a qualified podiatrist. 
+    </p>
+  </div>    
+</div>  
+
+
+ <div class="row">
+  <div class="large-3 columns"><h2>Home care tips</h2></div>
+  <div class="large-9 columns">
+    <p>
+ Daily care includes keeping feet clean and dry and skin soft and smooth. One should conduct daily inspection for red skin spots, cuts, sores, swelling or infected toe nails.  Also avoid extreme heat or cold on the feet, use sunscreen and wear shoes at the beach; keeping toe nails trimmed and feet free of callouses and corns. Do not cut corns or calluses or use plaster or liquid removers.      
+    </p>
+  </div>
+ </div>
+
+<div class="row">
+  <div class="large-3 columns"><h2>Shoes and socks</h2></div>
+  <div class="large-9 columns">
+    <p>
+Ill-fitting shoes or socks can lead to blisters, a major concern for podiatrists. The National Diabetes Education program suggest wearing soft lightly padded, seamless socks, wearing walking or athletic shoes that allow room for your toes. Never wear vinyl or plastic shoes and purchase new shoes at the end of the day when our feet are largest for the best fit.    
+    </p>
+  </div>
+ </div>  
+  
+<div class="row">
+  <div class="large-3 columns"><h2>Learn more</h2></div>
+  <div class="large-9 columns">
+    <ul class="bump no-decor">
+    <li><a href="http://www.diabetes.org/living-with-diabetes/complications/foot-complications/foot-care.html">American Diabetes Association</a></li>
+        <li><a href="http://www.ndep.nih.gov/media/NDEP4_TakeCareOfFeet_4c_508.pdf">National Diabetes Education Program</a> from the National Institutes of Health</li>
+    </ul>    
+  </div>
+ </div>   
+  
 <?php include("../includes/html-bottom.html"); ?>  
