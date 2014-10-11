f5fe1741f01bee622db6470d49e77605c6ca16f2
Christine McClure <camcclure@gmail.com>
2014-10-11 15:52:55 -0500

    image and text for ingrown townail page

--- ingrown-toenails/index.php
+++ ingrown-toenails/index.php
@@ -3,7 +3,37 @@
 <body>
 
 <?php include("../includes/header-and-nav.html"); ?>    
- 
 
-    
+ <div class="row">
+  <div class="large-12 columns"><h1>Ingrown toe nails</h1></div>
+ </div>
+  
+<div class="row">
+   <div class="large-12 columns">
+     <h2>Infections</h2>
+     <img src="../images/ingrown-bandaged-toes.jpg" alt="Photograph a pair of feet in black flip-flops. Both big toes are wrapped in bright pick bandages.">
+  </div>   
+</div>
+  
+<div class="row"> 
+  <div class="large-12 columns">
+     <p>
+There are many different types of foot infections, some simple, and others complex. They are categorized into 3 groups: soft tissue, bone, and diabetic foot infections. Signs of infection may include increased pain, swelling, redness, or warmth around the affected area red streaks extending from the affected area, pus draining from the area. Many events can be responsible for soft tissue infections. A description of soft tissue infections includes simple, moderate, and severe infections, Bone can be involved when an infectious process is present in the foot because of the close proximity to the skin and lack of a thick, soft tissue, protective layer throughout most parts of the foot. People with diabetes who get an infection are at high risk for complications such as ulcerations in the feet, which often lead to serious infection of the soft tissue and bones.       
+     </p>
+     
+<p>Dr. Ouska-Griffin uses a conservative two step approach.</p>
+
+  <p>First, the offending nail area is removed by an in-office sterile procedure with local numbing around the offending area. This first step does not remove the nail bed from which the nail grows. If the ingrown toe nail returns, which is common, or becomes chronic, a second step can be done at a later date - the offending nail section and a small section of the nail bed is removed by a simple in-office procedure. This second step results in a permanent fix in nearly all cases.</p>
+  
+  <p>Ways to avoid ingrown toenails:</p>
+        
+        <ul class="indented">
+          <li>Don't cut down the corners of the toenails.</li>
+          <li>Don't trim toenails too short.</li>
+          <li>Don't wear improperly fitting shoes such one with a very narrow or pointed toe area.</li>
+          <li>But….remember ingrown toenails may be genetic and require permanent removal.</li>
+        </ul>
+  </div>    
+</div>   
+
 <?php include("../includes/html-bottom.html"); ?>  
