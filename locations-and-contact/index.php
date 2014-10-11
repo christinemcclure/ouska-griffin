f2a48339d8130dc001e5aa96cbefe451cf6c59b8
Christine McClure <camcclure@gmail.com>
2014-10-11 16:36:58 -0500

    added h1 for locations page

--- locations-and-contact/index.php
+++ locations-and-contact/index.php
@@ -4,6 +4,8 @@
 
 <?php include("../includes/header-and-nav.html"); ?>    
  
-
+ <div class="row">
+  <div class="large-12 columns"><h1>Locations and contact</h1></div>
+ </div>
     
 <?php include("../includes/html-bottom.html"); ?>  
