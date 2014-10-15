bfa1e05992f71fec8045d5bced295842e8f75a96
unknown <christine@Christine-PC.ads.gl.iit.edu>
2014-10-15 09:36:07 -0500

    wrapped toenails image around text

--- ingrown-toenails/index.php
+++ ingrown-toenails/index.php
@@ -8,15 +8,10 @@
   <div class="large-12 columns"><h1>Ingrown toenails</h1></div>
  </div>
   
-<div class="row">
-   <div class="large-12 columns">
-     <h2>Infections</h2>
-     <img src="../images/ingrown-bandaged-toes.jpg" alt="Photograph a pair of feet in black flip-flops. Both big toes are wrapped in bright pick bandages.">
-  </div>   
-</div>
   
 <div class="row"> 
   <div class="large-12 columns">
+     <img class="inline-wrap" src="../images/ingrown-bandaged-toes.jpg" alt="Photograph a pair of feet in black flip-flops. Both big toes are wrapped in bright pick bandages.">
      <p>
 There are many different types of foot infections, some simple, and others complex. They are categorized into 3 groups: soft tissue, bone, and diabetic foot infections. Signs of infection may include increased pain, swelling, redness, or warmth around the affected area red streaks extending from the affected area, pus draining from the area. Many events can be responsible for soft tissue infections. A description of soft tissue infections includes simple, moderate, and severe infections, Bone can be involved when an infectious process is present in the foot because of the close proximity to the skin and lack of a thick, soft tissue, protective layer throughout most parts of the foot. People with diabetes who get an infection are at high risk for complications such as ulcerations in the feet, which often lead to serious infection of the soft tissue and bones.       
      </p>
