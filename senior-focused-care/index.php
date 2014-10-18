edd8507f6483ab3f479e24780f097f0d14bafd11
Christine McClure <camcclure@gmail.com>
2014-10-18 10:45:50 -0500

    added client text to link

--- senior-focused-care/index.php
+++ senior-focused-care/index.php
@@ -26,7 +26,7 @@ As we age, our feet typically change in structure and even size. Decades of stan
 Medicare typically covers any injury to the feet. Medicare covers ongoing routine foot care, every 60 days, for people with specific chronic conditions such as documented poor circulation or diabetes. Secondary insurance coverage is variable. Some plans cover 100% of the Medicare deductible and copay, other plans will have additional deductibles or limit coverage for routine care. However, any uncovered office copay amounts are usually small, under $20 in many cases.
         </p>
         
-        <p><a href="http://www.nia.nih.gov/health/publication/foot-care">Need text for this link</a></p>
+        <p><a href="http://www.nia.nih.gov/health/publication/foot-care">National Institutes on Aging</a></p>
   </div>    
 </div>  
 
