9edcc05188041ea1fc8aaa833a27ca608f97b013
Christine McClure <camcclure@gmail.com>
2014-10-18 10:04:57 -0500

    client changes for home page

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
 
 <div class="row">
 <div class="large-12 columns">
-<p>Our feet take us thousands of miles in our life - working, exercising, and traveling. We log an amazing 75,000 miles on our feet by the time we reach age 50 according to the American Podiatric Medical Association. Each foot contains 26 bones, 33 joints, and more than 120 muscles, ligaments, tendons, and nerves. These all work together to support the weight of the body, act as shock absorbers, keep us balanced, and push us forward with each stride. Because feet are small compared to the whole body, they receive an enormous impact with each footfall, adding up to hundreds of tons of weight every day. A sore foot can impact work, play and normal necessary daily routines and If not treated can stop all the action in our tracks. Our feet, while hidden by our newest stylish shoe, let us know loud and clear when they are not happy and healthy. That is where we come in - to help you keep your feet happy and healthy.</p>
+<p>Our feet take us thousands of miles in our life - working, exercising, and traveling. We log an amazing 75,000 miles on our feet by the time we reach age 50 according to the American Podiatric Medical Association. Each foot contains 26 bones, 33 joints, and more than 120 muscles, ligaments, tendons, and nerves. These all work together to support the weight of the body, act as shock absorbers, keep us balanced, and push us forward with each stride. Because feet are small compared to the whole body, they receive an enormous impact with each footfall. A sore foot can impact work, play and normal necessary daily routines and If not treated can stop all the action in our tracks. Our feet, while hidden by our newest stylish shoe, let us know loud and clear when they are not happy and healthy. That is where we come in - to help you keep your feet happy and healthy.</p>
 
-<p>Welcome to our podiatric practice. We have been providing comprehensive podiatric physician care in Chicago for over 25 years without interruption.</p>
+<p>Welcome to our podiatric practice. We have been providing comprehensive podiatric physician care in Chicago for over 25 years.</p>
 
 <p>How can we help you?</p>
 </div>  
@@ -29,13 +29,14 @@
 </div>
 <div class="large-9 columns">
   <p>
-Exceptional podiatric medicine begins with a thoughtful, and skilled diagnosis. Dr. Ouska-Griffin takes the time to listen to your description of your foot problem and concerns. We want to know where your feet take you in a normal day and what demands and stresses they face. We consider how other health issue might affect your feet. This personal and comprehensive approach is the core of a strong professional diagnostic evaluation. 
+Exceptional podiatric medicine begins with a thoughtful, and skilled diagnosis. Dr. Ouska-Griffin takes the time to listen to your description of your foot problem and concerns. We want to know where your feet take you in a normal day and what demands and stresses they face. We consider how other health issue might affect your feet. This personal and comprehensive approach is the core of a strong professional diagnostic evaluation. With our in-office x-ray capability we can 
+analyze and share results with you during your visit avoiding a trip to another facility and a second visit to learn our findings.
   </p>
   <p>
-    We take a hands on approach to patient treatment-our hands are on your feet evaluating and treating, not on a keyboard doing medical notes during your appointment. Once we have determined a diagnosis, we develop al conservative, progressive treatment path addressing the problem. We do not overtreat, overprescribe, nor overbill.
+    We take a hands on approach to patient treatment-our hands are on your feet evaluating and treating, not on a keyboard doing medical notes during your appointment. Once we have determined a diagnosis, we develop al conservative, progressive treatment path addressing the problem. We do not over treat, overprescribe, nor overbill.
   </p>
   <p>
-    We create our daily appointment schedule to respect your busy schedule. Most reception wait times are about five minutes and rarely over ten minutes. We do not overbook. A complex new patient visit can require 45 minutes to an hour and we schedule accordingly. We strive to make same day emergency appointments so long as they do not cause an inconvenience to our scheduled patients. With our in-office x-ray capability we can analyze and share results with you during your visit avoiding a trip to another facility and a second visit to learn our findings.
+    We create our daily appointment schedule to respect your busy schedule. Most reception wait times are rarely over ten minutes. We do not overbook. A complex new patient visit can require 45 minutes to an hour and we schedule accordingly. We strive to make same day emergency appointments so long as they do not cause an inconvenience to our scheduled patients.
   </p>
 </div>
 </div>
@@ -46,7 +47,7 @@ Exceptional podiatric medicine begins with a thoughtful, and skilled diagnosis. 
 </div>
 <div class="large-9 columns">
   <p>
-We are fortunate to serve a mix of long-time loyal patients and a growing stream of new patients, young adults to seniors, seeking personal care with a focus on them as a patient and as an individual. Our patients trust us to diagnose and treat a broad range of foot problems- acute and chronic, infections, injuries and structural. Our patients have a wide range of insurance plans. We are in network in most insurance plans. We accept both traditional and advantage Medicare insurance as well as plans under the Affordable Care Act. We do not accept HMO plans nor Medicaid. In a growing trend some patients must consider paying directly when their insurance plan does not provide coverage. We offer discounts from our standard fees on noncovered major procedures and custom orthotics, 50% or more, payable at time of service. [Smaller discounts may be available for noncovered office visits, x-rays, minor procedures and supplies.].
+We are fortunate to serve a mix of long-time loyal patients and a growing stream of new patients, young adults to seniors, seeking personal care with a focus on them as a patient and as an individual. Our patients trust us to diagnose and treat a broad range of foot problems- acute and chronic, infections, injuries and structural. Our patients have a wide range of insurance plans. We are in network in most insurance plans. We accept both traditional and Advantage Medicare insurance as well as plans under the Affordable Care Act. We do not accept HMO plans nor Medicaid. In a growing trend some patients must consider paying directly when their insurance plan does not provide coverage. We offer discounts from our standard fees on noncovered major procedures and custom orthotics, 50% or more, payable at time of service. [Smaller discounts may be available for noncovered office visits, x-rays, minor procedures and supplies.].
   </p>
 </div>
 </div>
