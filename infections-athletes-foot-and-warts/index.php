82229e750d2bfecbad973a54930a78022b2ffefa
Christine McClure <camcclure@gmail.com>
2014-10-18 10:31:02 -0500

    client changes toinfections page

--- infections-athletes-foot-and-warts/index.php
+++ infections-athletes-foot-and-warts/index.php
@@ -20,13 +20,13 @@ There are many different types of foot infections, some simple, and others compl
   
 <div class="row">
     <div class="large-3 columns">
-      <h2>Athlete's foot</h2>
+      <h2>Athlete's foot rash</h2>
   </div>    
   <div class="large-9 columns">
         <p>
 Athlete's foot is a common fungal skin infection that grows on or in the top layer of skin, especially in warm, wet places, such as between the toes. Athlete's foot can make your feet and the skin between your toes burn and itch. The skin may peel and crack. Athlete's foot spreads easily. You can get it by touching the toes or feet of a person who has it. But most often, people get it by walking barefoot on contaminated surfaces near swimming pools or in locker rooms. Athlete's foot may persist for long periods. Consequently, the infection may be spread by contaminated bed sheets or clothing to other parts of the body. The fungus can then grow in your shoes, especially if your shoes are so tight that air cannot move around your feet. Some people are more likely than others to get athlete's foot. After you have had athlete's foot, you are more likely to get it again.
         </p>
-        <p>See a podiatrist if the infection last over two weeks.</p>
+        <p>See a podiatrist if the rash lasts over one week.</p>
         <p>You can do much to prevent infection by practicing good foot hygiene:</p>
         <ul class="indented">
           <li>Wash feet daily with soap and water; dry carefully, especially between the toes </li>
@@ -45,7 +45,7 @@ Athlete's foot is a common fungal skin infection that grows on or in the top lay
   </div>    
   <div class="large-9 columns">
         <p>
-Plantar warts are caused by a viral infection, the human papillomavirus (HPV). The virus enters your body through tiny cuts, breaks or other weak spots on the bottom of your feet, especially the heel or ball of the foot. The many viruses that cause warts generally invade the skin through small or invisible cuts and abrasions. The plantar wart is often contracted by walking barefoot on dirty surfaces or littered ground or public, semiprivate showers where the virus is lurking. If left untreated, warts can grow to an inch or more in circumference and can spread into clusters of several warts. Plantar warts are contagious and are spread by touching, scratching, or even by contact with skin shed from another wart. The wart may also bleed, creating another route for spreading. Warts can be very resistant to treatment and have a tendency to reoccur. Over-the-counter preparations contain acids or chemicals that destroy skin cells. It takes an expert to destroy abnormal skin cells (warts) without also destroying surrounding healthy tissue. Self-treatment with such medications are effective in an estimated 50% of cases. Over the counter treatments should be avoided by people with diabetes and those with cardiovascular or circulatory disorders. Never use these medications in the presence of an bacterial active infection.
+Plantar warts are caused by a viral infection, the human papillomavirus (HPV). The virus enters your body through tiny cuts, breaks or other weak spots on the bottom of your feet, especially the heel or ball of the foot. The many viruses that cause warts generally invade the skin through small or invisible cuts and abrasions. The plantar wart is often contracted by walking barefoot on dirty surfaces or littered ground or public, semiprivate showers where the virus is lurking. If left untreated, warts can grow to an inch or more in circumference and can spread into clusters of several warts. Plantar warts are contagious and are spread by touching, scratching, or even by contact with skin shed from another wart. The wart may also bleed, creating another route for spreading. Warts can be very resistant to treatment and have a tendency to reoccur. Over-the-counter preparations contain acids or chemicals that destroy skin cells. It takes an expert to destroy abnormal skin cells (warts) without also destroying surrounding healthy tissue. Podiatrists treat warts using multiple clinical methods including cryosurgery or permanent removal. Over the counter treatments should be avoided by people with diabetes and those with cardiovascular or circulatory disorders. Never use these medications in the presence of a bacterial active infection.
         </p>
   </div>    
 </div>   
