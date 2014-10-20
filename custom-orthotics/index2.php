ab229a7122a2ec114f997c45113a5fa7f2612f0c
Christine McClure <camcclure@gmail.com>
2014-10-20 14:52:49 -0500

    added second page without orthotics flowchart for client to see

---
+++ custom-orthotics/index2.php
@@ -0,0 +1,70 @@
+<?php include("../includes/html-head.html"); ?>
+  
+<body>
+
+<?php include("../includes/header-and-nav.html"); ?>    
+
+ <div class="row">
+  <div class="large-12 columns"><h1>Custom orthotics</h1></div>
+ </div>
+  
+  <div class="row">
+  <div class="large-4 columns">
+    <img src="../images/orthotics-custom.jpg" alt="">
+  </div> 
+    <div class="large-8 columns">
+    <p>There are three major elements to podiatric prescribed custom orthotics:</p>
+    <ol class="indent-left">
+      <li>Designed by a podiatrist through a diagnosis of your specific biomechanics</li>
+      <li>Type, materials, and special enhancements guided by a discussion between you and your podiatrist</li>
+      <li>Fabricated by a specialized lab from a detailed prescription, often enhanced by direct communication</li>
+    </ol>      
+    </div>
+</div>  
+
+<div class="row">
+  <div class="large-4 columns">
+  <h4>Weaknesses of retail or foam molded orthotics</h4>
+  </div>
+  <div class="large-8 columns">
+    <img class="inline-wrap-right" src="../images/orthotics-insole.jpg" alt="Drawn image of a mass-market replacement insole, with lines on where to trim to match standard shoe sizes.">
+        <p>
+         Over the counter supports, even “fitted ones” will not relieve pain as well as a custom orthotic made specifically for you and matched for intended use. Image generated and foam impressions made with the patient standing only recreate the deformity with the foot under pressure. The skill is in the non-weight bearing, neutral position, cast impression controlled by an experienced podiatrist. 
+        </p>
+  </div>    
+</div>   
+
+<div class="row">
+  <div class="large-4 columns">
+  <h4>Multiple custom orthotics for different uses</h4>
+  </div>
+  <div class="large-8 columns">
+        <p>
+         There are multiple styles of custom orthotics for dress, work, and sport shoes. Many insurance companies will cover multiple sets.
+        </p>
+  </div>    
+</div>  
+  
+<div class="row">
+  <div class="large-4 columns">
+  <h4>One set of orthotics are not for a lifetime</h4>
+  </div>
+  <div class="large-8 columns">
+        <p>
+         Like shoes, custom orthotics are not designed for a lifetime of use. What once worked may not be doing the job today. Our foot’s biomechanics change as we age. Changes in your daily activities may place new demands on your feet as well. Either may warrant an evaluation of your current orthotic needs.  
+        </p>
+  </div>    
+</div>  
+  
+<div class="row">
+  <div class="large-4 columns">
+  <h4>Custom orthotic refurbishing</h4>
+  </div>
+  <div class="large-8 columns">
+        <p>
+         Maybe the structural benefits of your current orthotics are fine but they need refurbishing. The replacement of top covers both shock absorbing and leather materials enhance the comfort of your supports. Wearing ripped or frayed top covers can cause blisters and bruising to soles and toes. Refurbishing is inexpensive and does not require new casting.
+        </p>
+  </div>    
+</div>    
+  
+<?php include("../includes/html-bottom.html"); ?>  
