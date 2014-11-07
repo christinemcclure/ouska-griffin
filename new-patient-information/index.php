23415e29a9de4af23d361fb47f31abda762b1e37
Christine McClure <camcclure@gmail.com>
2014-11-07 15:23:02 -0600

    added form links to new patient file

--- new-patient-information/index.php
+++ new-patient-information/index.php
@@ -5,7 +5,10 @@
 <?php include("../includes/header-and-nav.html"); ?>    
  
  <div id="maincontent" class="row">
-  <div class="large-12 columns"><h1>New patient information</h1></div>
+  <div class="large-12 columns"><h1>New patient information</h1>
+  <p>Patient registration form <a href="patient-registration-form.pdf">(download pdf)</a></p>
+  <p>Patient medical information form <a href="patient-medical-information-form.pdf">(download pdf)</a></p>
+  </div>
  </div>
     
 <?php include("../includes/html-bottom.html"); ?>  
