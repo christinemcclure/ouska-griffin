dfddb2092c8b379be11780ad8a574937c61f1174
Christine McClure <camcclure@gmail.com>
2014-11-08 09:46:06 -0600

    included sticky footer js snippet from http://foundation.zurb.com/forum/posts/629-sticky-footer

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).bind("load", function () {
+    var footer = $("#footer");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});
