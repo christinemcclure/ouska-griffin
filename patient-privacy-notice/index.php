f3b0cea5fdf6888691aadf69cb36f166d22c4547
Christine McClure <camcclure@gmail.com>
2015-02-14 08:52:18 -0600

    added privacy notice PDF to menu and page

---
+++ patient-privacy-notice/index.php
@@ -0,0 +1,14 @@
+<?php include("../includes/html-head.html"); ?>
+  
+<body>
+
+<?php include("../includes/header-and-nav.html"); ?>    
+ 
+ <div id="maincontent" class="row">
+  <div class="large-12 columns"><h1>Patient Privacy Notice</h1>
+  <p class="emphasis bump">Patient Privacy Notice<a href="ouska-privacy-2015.pdf"> (download pdf)</a></p>
+
+  </div>
+ </div>
+    
+<?php include("../includes/html-bottom.html"); ?>  
