d5cd49a09e024792fee947ef669fd81e15b79577
Christine McClure <camcclure@gmail.com>
2018-10-28 10:54:38 -0500

    adjusted formatting and added photo

--- board-certification/index.php
+++ board-certification/index.php
@@ -11,10 +11,10 @@
  </div>
   
   <div class="row">
-  <div class="large-4 columns">
+  <div class="large-3 columns">
     <p class="emphasis">Board Certification</p>
   </div> 
-    <div class="large-8 columns">
+    <div class="large-9 columns">
 
     <p>
       Dr. Ouska-Griffin has been board certified by the American Board of Podiatric Medicine since 1996.  As a board certified podiatric physician she is required to recertify her board accreditation every 10 years. She has successfully completed recertification throughout her career.
@@ -23,10 +23,11 @@
 </div>  
 
 <div class="row">
-  <div class="large-4 columns">
+  <div class="large-3 columns">
   <p class="emphasis">Continuing Medical Education</p>
   </div>
-  <div class="large-8 columns">
+  <div class="large-9 columns">
+    <img class="inline-wrap-right" src="../images/medical-education.jpg" alt="Photo of an online medical lecture">    
     <p>Competent physician care requires a physician to continually learn throughout her/his career.</p>
     <p>The Accreditation Council for Continuing Medical Education defines continuing medical education as: "[E]ducational activities which serve to maintain, develop, or increase the knowledge, skills, and professional performance and relationships that a physician uses to provide services for patients, the public, or the profession."</p>
     <p>Dr. Ouska-Griffin has completed an estimated 1,400 hours of certified continuing medical education in her career. Most states require the physician to complete a complete a minimum hourly total of approved certified CME courses annually. (Illinois has one of the most rigorous podiatric CME requirements in the nation, on par with MDs). Dr. Ouska-Griffin has always met or exceeded this requirement.</p>
