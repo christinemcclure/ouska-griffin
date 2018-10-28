eb0c649970c9f81b99aa4d2a371b9f9e87d99f4d
Christine McClure <camcclure@gmail.com>
2018-10-28 11:08:58 -0500

    added patient benefits page and navigation

---
+++ patient-benefits/index.php
@@ -0,0 +1,69 @@
+<?php include("../includes/html-head1.html"); ?>
+  <title>Dr. Florence Ouska-Griffin - Patient Benefits of Our Independent Podiatric Practice</title>
+<?php include("../includes/html-head2.html"); ?>  
+  
+<body>
+
+<?php include("../includes/header-and-nav.html"); ?>    
+
+ <div id="maincontent" class="row">
+  <div class="large-12 columns">
+    <h1>Patient Benefits of Our Independent Podiatric Practice</h1>
+    <p>Our essential patient benefits:</p>
+  </div>
+ </div>
+  
+  <div class="row">
+  <div class="large-3 columns">
+    <p class="emphasis">Flexibility in scheduling and minimal waiting</p>
+  </div> 
+    <div class="large-9 columns">
+
+    <p>
+Our practice usually can book a patient within a day or two and on the same day if there is an emergency. It is common to wait several weeks or months to see a physician in a hospital clinic. Reception wait times are usually 10 minutes or less at our office.
+    </p>
+    </div>
+</div>  
+
+<div class="row">
+  <div class="large-3 columns">
+  <p class="emphasis">Lower Fees</p>
+  </div>
+  <div class="large-9 columns">
+    <p>his matters as most patients have employer driven high deductible insurance plans and/or must pay balances out of their own pocket. Our payments are determined by the insurance plans in which we participate under contract (We are in most major insurance plans.) Independent practice approved insurance fees are considerably less than those insurance fees approved for hospitals and outpatient centers. Our cash fees are set around major insurer standard fees for physician offices.</p>
+  </div>    
+</div>   
+
+<div class="row">
+  <div class="large-3 columns">
+  <p class="emphasis">Personalized Care</p>
+  </div>
+  <div class="large-9 columns">
+    <p>
+      In a large hospital clinic, patients may see a different physician in subsequent visits or not even see the physician at all but rather one of many nonphysician associates. Dr. Ouska-Griffin takes time to learn her patient’s individual medical history, current problems and treatment goals. Every patient is interviewed and examined by Dr. Ouska-Griffin at every visit. Some patients have been with Dr. Ouska-Griffin for decades and have been treated for multiple foot problems as they arise.
+    </p>
+  </div>    
+</div>   
+<div class="row">
+  <div class="large-3 columns">
+  <p class="emphasis">Better Care</p>
+  </div>
+  <div class="large-9 columns">
+    <p>Ouska-Griffin takes the time to develop a treatment plan together with the patient. Treatment is not based some one size fits all generic plan. A recent article discussed three research studies that suggest that small practices may provide better care when compared to larger ones. (Do small practices provide better patient care? Leigh Page, Freelance healthcare writer, Chicago, Illinois, in Medscape, November 19, 2014.). We agree. Our patients’ praise and testimony is evidence of solid patient satisfaction.
+      
+    </p>
+  </div>    
+</div>   
+<div class="row">
+  <div class="large-3 columns">
+  <p class="emphasis">A physician who enjoys treating patients</p>
+  </div>
+  <div class="large-9 columns">
+    <p>A study suggests that physicians in small practices may have less burnout compared to those in hospitals. (Medscape Medical News: Burnout Rare Among Physicians in Small Practices Diana Phillips July 10, 2018).
+</p>
+<p>Dr. Ouska-Griffin looks forward to coming to work each day and focusing on the clinical challenges presented to her.
+</p>
+  </div>    
+</div>   
+
+  <?php include("../includes/html-bottom.html"); ?>  
